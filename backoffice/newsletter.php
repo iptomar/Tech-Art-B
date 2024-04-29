83e2d3781aadc31ba3660a4fe0ebf2b0009daff0
aluno21530 <aluno21530@ipt.pt>
2024-04-29 22:57:47 +0100

    Limpeza do campo de corpo da memsagem enviada por email

--- backoffice/newsletter.php
+++ backoffice/newsletter.php
@@ -27,23 +27,9 @@ try {
 
     // Conteúdo do e-mail
     $mail->isHTML(true);
-    $mail->Subject = 'Admissão aceita';
-    $mail->Body    = "Prezado(a) [Nome do Utilizador],<br><br>"
-        . "Saudações da Administração da TecnArt!<br><br>"
-        . "Gostaríamos de informá-lo(a) que sua admissão foi aceita em nosso site. Como parte do processo de criação de conta, geramos uma nova palavra-passe para você.<br><br>"
-        . "Aqui está sua nova palavra-passe: [Nova Senha]<br><br>"
-        . "Por favor, lembre-se de manter esta senha em um local seguro e não compartilhá-la com ninguém. Recomendamos que você faça login em sua conta o mais rápido possível e altere a palavra-passe para uma de sua preferência.<br><br>"
-        . "Se precisar de assistência adicional ou tiver alguma dúvida, não hesite em entrar em contato conosco. Estamos aqui para ajudar!<br><br>"
-        . "Atenciosamente,<br>"
-        . "Administração TechnArt";
-    $mail->AltBody = "Prezado(a) [Nome do Utilizador],<br><br>"
-        . "Saudações da Administração da TecnArt!<br><br>"
-        . "Gostaríamos de informá-lo(a) que sua conta foi criada com sucesso em nosso site. Como parte do processo de criação de conta, geramos uma nova senha para você.<br><br>"
-        . "Aqui está sua nova senha: [Nova Senha]<br><br>"
-        . "Por favor, lembre-se de manter esta senha em um local seguro e não compartilhá-la com ninguém. Recomendamos que você faça login em sua conta o mais rápido possível e altere a senha para uma de sua preferência.<br><br>"
-        . "Se precisar de assistência adicional ou tiver alguma dúvida, não hesite em entrar em contato conosco. Estamos aqui para ajudar!<br><br>"
-        . "Atenciosamente,<br>"
-        . "Administração TechnArt";
+    $mail->Subject = 'Notícias TechnArt';
+    $mail->Body    = "";
+    $mail->AltBody = "";
     // Enviar o e-mail
     $mail->send();
     echo 'E-mail enviado com sucesso!';
