351306a5b3f0c655aef81bbf2dd43cbc89d2fcfc
aluno21530 <aluno21530@ipt.pt>
2024-05-09 22:55:37 +0100

    commit para remover credenciais de acesso

--- backoffice/admissoes/details.php
+++ backoffice/admissoes/details.php
@@ -62,7 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['admitir_investigador']
                     $mail->Host = 'smtp.gmail.com';
                     $mail->SMTPAuth = true;
                     $mail->Username = 'tecnartadm@gmail.com';
-                    $mail->Password = 'nqfi ywzk jboh hpim';
+                    $mail->Password = '';
                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                     $mail->Port = 587;
 
